3f603149ed3ff9c65da696dddb79c9f05570f153
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-11 18:44:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,11 @@
 
 def key_for_min_value(name_hash)
   min_value = 0
-
+  min_key = nil
   name_hash.each do |key,value|
     if min_value == 0 || min_value > value
-
+      min_value = value
+      min_key = key
 
 end
 end
